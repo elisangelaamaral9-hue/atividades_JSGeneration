68411051b75d3c324f6e0054c3cf6e5a765a463e
elisangelaamaral9-hue <elisangela.amaral9728@gmail.com>
2025-10-29 16:29:48 -0300

    Update atividade03.js

--- atividade03.js
+++ atividade03.js
@@ -3,16 +3,16 @@ const leia = require('readline-sync')
 
 let salario, desconto, extra, adicional
 
-console.log = ("Digite o seu salario: ")
+console.log("Digite o seu salario: ")
 salario = leia.questionFloat()
 
-console.log = ("Digite o desconto: ")
+console.log("Digite o desconto: ")
 desconto = leia.questionFloat()
 
-console.log = ("Digite as horas extras: ")
+console.log("Digite as horas extras: ")
 extra = leia.questionFloat()
 
-console.log = ("Digite seu adicional noturno: ")
+console.log("Digite seu adicional noturno: ")
 adicional = leia.questionFloat()
 
 soma = (salario+(extra*5)+adicional)-desconto
